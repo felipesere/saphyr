9b9498e2ba1225c2f099b03cf5bf318cceb2a02d
Felipe Sere <github@felipesere.com>
2025-08-09 22:52:24 +0100

    Add remaining SafelyIndex impls

--- saphyr/src/index.rs
+++ saphyr/src/index.rs
@@ -1,4 +1,4 @@
-use crate::{LoadableYamlNode, MarkedYaml, MarkedYamlOwned};
+use crate::{annotated, MarkedYaml, MarkedYamlOwned, Yaml, YamlData, YamlDataOwned, YamlOwned};
 
 /// A trait to safely index into a structure with an `Accessor`.
 /// This will never panic and return an `Option::None` on failure.
@@ -19,6 +19,37 @@ impl<YAML, T: SafelyIndex<YAML>> SafelyIndex<YAML> for &T {
     }
 }
 
+impl SafelyIndex<YamlOwned> for YamlOwned {
+    fn get(&self, key: impl Accessor<YamlOwned>) -> Option<&YamlOwned> {
+        key.index_into(self)
+    }
+}
+
+impl<'input> SafelyIndex<Yaml<'input>> for Yaml<'input> {
+    fn get(&self, key: impl Accessor<Yaml<'input>>) -> Option<&Yaml<'input>> {
+        key.index_into(self)
+    }
+}
+
+impl<N> SafelyIndex<YamlDataOwned<N>> for YamlDataOwned<N>
+where
+    N: annotated::AnnotatedNodeOwned + From<Self>,
+{
+    fn get(&self, key: impl Accessor<YamlDataOwned<N>>) -> Option<&YamlDataOwned<N>> {
+        key.index_into(self)
+    }
+}
+
+impl<'input, N> SafelyIndex<YamlData<'input, N>> for YamlData<'input, N>
+where
+    N: annotated::AnnotatedNode + From<Self>,
+{
+    fn get(&self, key: impl Accessor<YamlData<'input, N>>) -> Option<&YamlData<'input, N>> {
+        key.index_into(self)
+    }
+}
+
+// These will have to be cleverer!
 impl<'a> SafelyIndex<MarkedYaml<'a>> for MarkedYaml<'a> {
     fn get(&self, key: impl Accessor<MarkedYaml<'a>>) -> Option<&MarkedYaml<'a>> {
         key.index_into(self)
@@ -39,41 +70,25 @@ pub trait Accessor<Y> {
 
 impl Accessor<MarkedYamlOwned> for usize {
     fn index_into(self, yaml: &MarkedYamlOwned) -> Option<&MarkedYamlOwned> {
-        if yaml.is_sequence() {
-            yaml.data.as_sequence_get(self)
-        } else {
-            None
-        }
+        yaml.data.as_sequence_get(self)
     }
 }
 
 impl<'input> Accessor<MarkedYaml<'input>> for usize {
     fn index_into<'y>(self, yaml: &'y MarkedYaml<'input>) -> Option<&'y MarkedYaml<'input>> {
-        if yaml.is_sequence() {
-            yaml.data.as_sequence_get(self)
-        } else {
-            None
-        }
+        yaml.data.as_sequence_get(self)
     }
 }
 
 impl Accessor<MarkedYamlOwned> for &str {
     fn index_into(self, yaml: &MarkedYamlOwned) -> Option<&MarkedYamlOwned> {
-        if yaml.is_mapping() {
-            yaml.data.as_mapping_get(self)
-        } else {
-            None
-        }
+        yaml.data.as_mapping_get(self)
     }
 }
 
 impl<'input> Accessor<MarkedYaml<'input>> for &str {
     fn index_into<'y>(self, yaml: &'y MarkedYaml<'input>) -> Option<&'y MarkedYaml<'input>> {
-        if yaml.is_mapping() {
-            yaml.data.as_mapping_get(self)
-        } else {
-            None
-        }
+        yaml.data.as_mapping_get(self)
     }
 }
 
