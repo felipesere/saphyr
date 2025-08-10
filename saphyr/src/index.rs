daf9f66f121906d2707687d789b98e2c1c0b915f
Felipe Sere <github@felipesere.com>
2025-08-10 20:14:18 +0100

    Implement index traits as a macro

--- saphyr/src/index.rs
+++ saphyr/src/index.rs
@@ -67,39 +67,3 @@ pub trait Accessor<Y> {
     /// something important
     fn index_into(self, yaml: &Y) -> Option<&Y>;
 }
-
-impl Accessor<MarkedYamlOwned> for usize {
-    fn index_into(self, yaml: &MarkedYamlOwned) -> Option<&MarkedYamlOwned> {
-        yaml.data.as_sequence_get(self)
-    }
-}
-
-impl<'input> Accessor<MarkedYaml<'input>> for usize {
-    fn index_into<'y>(self, yaml: &'y MarkedYaml<'input>) -> Option<&'y MarkedYaml<'input>> {
-        yaml.data.as_sequence_get(self)
-    }
-}
-
-impl Accessor<MarkedYamlOwned> for &str {
-    fn index_into(self, yaml: &MarkedYamlOwned) -> Option<&MarkedYamlOwned> {
-        yaml.data.as_mapping_get(self)
-    }
-}
-
-impl<'input> Accessor<MarkedYaml<'input>> for &str {
-    fn index_into<'y>(self, yaml: &'y MarkedYaml<'input>) -> Option<&'y MarkedYaml<'input>> {
-        yaml.data.as_mapping_get(self)
-    }
-}
-
-impl Accessor<MarkedYamlOwned> for String {
-    fn index_into(self, yaml: &MarkedYamlOwned) -> Option<&MarkedYamlOwned> {
-        self.as_str().index_into(yaml)
-    }
-}
-
-impl<'input> Accessor<MarkedYaml<'input>> for String {
-    fn index_into<'y>(self, yaml: &'y MarkedYaml<'input>) -> Option<&'y MarkedYaml<'input>> {
-        self.as_str().index_into(yaml)
-    }
-}
