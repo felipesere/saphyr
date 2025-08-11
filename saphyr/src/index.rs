f5181bff1fe551d9fbda4fb5205e536d73198485
Felipe Sere <github@felipesere.com>
2025-08-11 08:32:20 +0100

    Create impls for SafelyIndex via Macro

--- saphyr/src/index.rs
+++ saphyr/src/index.rs
@@ -1,5 +1,3 @@
-use crate::{annotated, MarkedYaml, MarkedYamlOwned, Yaml, YamlData, YamlDataOwned, YamlOwned};
-
 /// A trait to safely index into a structure with an `Accessor`.
 /// This will never panic and return an `Option::None` on failure.
 pub trait SafelyIndex<X = Self> {
@@ -45,69 +43,5 @@ impl<Z: SafelyIndex> SafelyIndex<Z> for Option<&Z> {
 impl<T: SafelyIndex> SafelyIndex<T> for &T {
     fn get(&self, key: impl Into<Accessor>) -> Option<&T> {
         (*self).get(key)
-    }
-}
-
-impl SafelyIndex for YamlOwned {
-    fn get(&self, key: impl Into<Accessor>) -> Option<&YamlOwned> {
-        match key.into() {
-            Accessor::Field(f) => self.as_mapping_get(f.as_str()),
-            Accessor::Index(i) => self.as_sequence_get(i),
-        }
-    }
-}
-
-impl<'input> SafelyIndex for Yaml<'input> {
-    fn get(&self, key: impl Into<Accessor>) -> Option<&Yaml<'input>> {
-        match key.into() {
-            Accessor::Field(f) => self.as_mapping_get(f.as_str()),
-            Accessor::Index(i) => self.as_sequence_get(i),
-        }
-    }
-}
-
-impl<N> SafelyIndex<N> for YamlDataOwned<N>
-where
-    N: annotated::AnnotatedNodeOwned
-        + From<Self>
-        + PartialEq<<N as annotated::AnnotatedNodeOwned>::HashKey>,
-{
-    fn get(&self, key: impl Into<Accessor>) -> Option<&N> {
-        match key.into() {
-            Accessor::Field(f) => self.as_mapping_get(f.as_str()),
-            Accessor::Index(i) => self.as_sequence_get(i),
-        }
-    }
-}
-
-impl<N> SafelyIndex<N> for YamlData<'_, N>
-where
-    N: annotated::AnnotatedNode
-        + From<Self>
-        + for<'a> PartialEq<<N as annotated::AnnotatedNode>::HashKey<'a>>,
-{
-    fn get(&self, key: impl Into<Accessor>) -> Option<&N> {
-        match key.into() {
-            Accessor::Index(i) => self.as_sequence_get(i),
-            Accessor::Field(f) => self.as_mapping_get(f.as_str()),
-        }
-    }
-}
-
-impl<'a> SafelyIndex for MarkedYaml<'a> {
-    fn get(&self, key: impl Into<Accessor>) -> Option<&MarkedYaml<'a>> {
-        match key.into() {
-            Accessor::Field(f) => self.data.as_mapping_get(f.as_str()),
-            Accessor::Index(i) => self.data.as_sequence_get(i),
-        }
-    }
-}
-
-impl SafelyIndex for MarkedYamlOwned {
-    fn get(&self, key: impl Into<Accessor>) -> Option<&MarkedYamlOwned> {
-        match key.into() {
-            Accessor::Field(f) => self.data.as_mapping_get(f.as_str()),
-            Accessor::Index(i) => self.data.as_sequence_get(i),
-        }
     }
 }
